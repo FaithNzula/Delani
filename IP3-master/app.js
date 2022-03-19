92335eb2a3ee0de35ba9d84ed566f52956ee9d2d
FaithNzula <faithnzula1230@gmail.com>
2022-03-19 04:35:20 +0300

    name too long conditions/name too short

--- IP3-master/app.js
+++ IP3-master/app.js
@@ -7,3 +7,13 @@ const nameTooLong = document.querySelector('#name-too-long');
 form.onsubmit = (e) => {
     e.preventDefault();
 }
+name.oninput = (e) => {
+    nameTooShort.hidden = true;
+    nameTooLong.hidden = true;
+    if (e.srcElement.validity.nameTooShort) {
+        nameTooShort.hiden = false;
+    }
+}
+if (e.srcElement.validity.tooLong) {
+    nameTooLong.hidden = false;
+  }
